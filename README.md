5eaa51ccf4d3280215f0bed3de6fd0594d694a41
Grant Wilson <grantm.wilsonii@gmail.com>
2022-04-25 21:29:41 -0400

    add responsiveness udpates

--- README.md
+++ README.md
@@ -2,10 +2,5 @@
 
 ## TODO
 
-- Responsive
-  - Hide search
-  - resize sections with aspect ratio
-  - rearrange grid at sm/md breakpoint
-- text selection
 - links and titles to paintings
 - about footer with gradient
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["src/*.{js,css,html}"],
   theme: {
-    extend: { height: { 192: "48rem" }, scale: { sm: "101%" } },
+    extend: {
+      height: { 192: "48rem" },
+      scale: { sm: "101%" },
+      aspectRatio: { wide: "2 / 1" },
+    },
   },
   plugins: [],
 };
